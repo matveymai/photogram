1e12325fd8db6bdeab2614ca4b22e3cc4d719942
Matt <mayorov.matvey@list.ru>
2020-09-21 20:20:13 +0300

    upload photo function and css styles end of 7 lesson

--- src/main/java/com/example/demo_web_app/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo_web_app/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/","/registration")
+                .antMatchers("/","/registration","/static/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/example/demo_web_app/controller/GreetingController.java
+++ src/main/java/com/example/demo_web_app/controller/GreetingController.java
@@ -4,20 +4,28 @@ import com.example.demo_web_app.model.Message;
 import com.example.demo_web_app.model.User;
 import com.example.demo_web_app.repos.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class GreetingController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value(value = "${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Model model) {
@@ -45,9 +53,26 @@ public class GreetingController {
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Model model){
+                      @RequestParam("file") MultipartFile file,
+                      Model model) throws IOException {
 
         Message message = new Message(text,tag,user);
+
+        if (file!=null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.addAttribute("messages",messages);
