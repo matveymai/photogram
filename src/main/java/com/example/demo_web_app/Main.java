f4085ee14fc3ea221b037022f42cff3ec0455216
Matt <mayorov.matvey@list.ru>
2020-09-22 19:48:47 +0300

    added some bootstrap styles and extra access for users and admins a half of 8 lesson

--- src/main/java/com/example/demo_web_app/Main.java
+++ src/main/java/com/example/demo_web_app/Main.java
@@ -2,21 +2,14 @@ package com.example.demo_web_app;
 
 import com.example.demo_web_app.model.Role;
 import com.example.demo_web_app.model.User;
+import org.hibernate.Session;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-    User user = new User();
-    System.out.println(Arrays.toString(Role.values()));
-    EnumSet<Role> roles = EnumSet.allOf(Role.class);
-    user.setRoles(roles);
-    System.out.println(user.getRoles());
 
-        for (Role role : roles) {
-            System.out.println(role);
-        }
 
     }
 }
